c5b6db8733940ac5ddee13065b16144860bb2477
Rodrigo farias <rodrigofarias23d@gmail.com>
2016-11-22 15:57:31 -0300

    Convertendo para always_ff

--- Primeiro_Contador/Primeiro_Contador.sv
+++ Primeiro_Contador/Primeiro_Contador.sv
@@ -1,7 +1,7 @@
 module Primeiro_Contador(input logic R, CLK, output logic[3:0] O);
 	logic DIR;
 
-	always_comb @(posedge CLK or posedge R)
+	always_ff @(posedge CLK or posedge R)
 		if(R) begin
 			O <= 4'd0;
 			DIR <= 1'b0;
@@ -10,8 +10,14 @@ module Primeiro_Contador(input logic R, CLK, output logic[3:0] O);
 				O <= O + 4'd1;
 			else
 				O <= O - 4'd1;
-			if(O == 4'd0 || O == 4'd15)
-				DIR <= ~DIR;
+			if (O == 4'd15) begin			
+				O <= O - 4'd1;
+				DIR <= 1;
+			end else
+				if(O == 4'd0) begin
+					O <= O + 4'd1;
+					DIR <= 0;
+				end
 		end
 		
 endmodule
