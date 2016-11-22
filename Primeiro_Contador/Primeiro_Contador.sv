4b8103167ca3dbaa3c327b788f727ea6515ab276
Rodrigo Farias <rodrigofarias23d@gmail.com>
2016-11-22 00:12:39 -0300

    Corrigindo primeiro contador

--- Primeiro_Contador/Primeiro_Contador.sv
+++ Primeiro_Contador/Primeiro_Contador.sv
@@ -1,16 +1,17 @@
 module Primeiro_Contador(input logic R, CLK, output logic[3:0] O);
 	logic DIR;
-	always @(posedge CLK or posedge R) begin
-		if (R) begin
-			DIR <= 1'd0;
-			O <= 4'd0;
+
+	always @(posedge CLK or posedge R)
+		if(R) begin
+			O = 4'd0;
+			DIR = 1'b0;
 		end else begin
-			if (DIR == 0)
-				O <= O + 4'd1;
+			if(DIR == 0)
+				O = O + 4'd1;
 			else
-				O <= O - 4'd1;
-			if (O == 4'd15 || O == 4'd0)
+				O = O - 4'd1;
+			if(O == 4'd0 || O == 4'd15)
 				DIR = ~DIR;
 		end
-	end
+		
 endmodule
