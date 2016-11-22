a7a484d8678104195dded59c772208bb583fcf67
Rodrigo farias <rodrigofarias23d@gmail.com>
2016-11-22 15:15:04 -0300

    Convertendo para always combinacional

--- Primeiro_Contador/Primeiro_Contador.sv
+++ Primeiro_Contador/Primeiro_Contador.sv
@@ -1,17 +1,17 @@
 module Primeiro_Contador(input logic R, CLK, output logic[3:0] O);
 	logic DIR;
 
-	always @(posedge CLK or posedge R)
+	always_comb @(posedge CLK or posedge R)
 		if(R) begin
-			O = 4'd0;
-			DIR = 1'b0;
+			O <= 4'd0;
+			DIR <= 1'b0;
 		end else begin
 			if(DIR == 0)
-				O = O + 4'd1;
+				O <= O + 4'd1;
 			else
-				O = O - 4'd1;
+				O <= O - 4'd1;
 			if(O == 4'd0 || O == 4'd15)
-				DIR = ~DIR;
+				DIR <= ~DIR;
 		end
 		
 endmodule
